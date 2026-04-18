527acab93b6e6f6e2beea3d61c17723f860d63a3
Jesús Cuautle <j.cuautle016@gmail.com>
2026-04-17 19:21:05 -0600

    feat: Implementa patron singleton para entity manager factory

---
+++ src/main/java/com/jpa/eclipselink/util/JPAUtil.java
@@ -0,0 +1,28 @@
+package com.jpa.eclipselink.util;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+
+    private static final EntityManagerFactory entityManagerFactory = crearEntityManagerFactory();
+
+    private static EntityManagerFactory crearEntityManagerFactory(){
+        try {
+            return Persistence.createEntityManagerFactory("CursoJPAPU");
+        }catch (Throwable ex){
+            System.err.println("No se pudo crear la fábirca: " + ex.getMessage());
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+
+    public static void close(){
+        if(entityManagerFactory != null){
+            entityManagerFactory.close();
+        }
+    }
+}
